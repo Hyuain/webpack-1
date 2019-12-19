1a64213dae57af45b5a89dd8c54f7ac2181c8523
Hyuain <Hyuain97@163.com>
2019-12-19 21:43:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import png from './assets/1.png'
 
 const div = document.querySelector('#app')
 
-div.innerHTML = `
-  <img src="${png}">
-`
+// div.innerHTML = `
+//   <img src="${png}">
+// `
 const button = document.createElement('button')
 button.innerHTML = '懒加载'
 button.onclick= () => {
